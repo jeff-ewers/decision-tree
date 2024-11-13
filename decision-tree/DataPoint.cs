f32171da9a26a0ac9fabe7b6fafa5ccbc537e053
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-11-13 15:10:27 -0600

    Make DataPoint public, handle nullable properties

---
+++ decision-tree/DataPoint.cs
@@ -0,0 +1,8 @@
+namespace DecisionTree 
+{
+    public class DataPoint
+    {
+        public required double[] Features { get; set; }
+        public required string Label { get; set; }
+    }
+}
--- decision-tree/DecisionTree.cs
+++ decision-tree/DecisionTree.cs
@@ -8,14 +8,14 @@ namespace DecisionTree
         public class Node
         {
             public bool IsLeaf { get; set; } // whether this is final decision point
-            public string Label { get; set; }   // for leaf nodes (final decision)
+            public string? Label { get; set; }   // for leaf nodes (final decision)
             public int FeatureIndex { get; set; } // feature we're splitting on
             public double SplitValue { get; set; } // value to split at
-            public Node Left { get; set; } // for values <= split val
-            public Node Right { get; set; } // for values > split val
+            public Node? Left { get; set; } // for values <= split val
+            public Node? Right { get; set; } // for values > split val
         }
     // main tree properties
-    private Node Root;
+    private Node? Root;
     private int maxDepth;
     // constructor
     public DecisionTree(int maxDepth = 5) 
